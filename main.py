1db2d75e93718ce69df76156b87e083acf7e2d56
nicholaswanipaul <nicholaswanipaul@gmail.com>
2023-07-11 21:39:40 +0200

    Update main.py
    o;io

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ with col1:
    # dataframe_price_year = price_data[new_category_year].groupby(['commodity', 'unit_price'])['Year'].sum()
     #dataframe_price_year = dataframe_price_year.reset_index()
     # Plot the responses for different events and regions
-    fig, plot=sns.lineplot(x="Year", y="unit_price",hue="category",data=price_data)
+    plot=sns.lineplot(x="Year", y="unit_price",hue="category",data=price_data)
     #st.dataframe(dataframe_price_year, width=400)
     rcParams['figure.figsize'] = 15,8
     st.pyplot(plot.get_figure())
